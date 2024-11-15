dae8018457f4b9c7d058298920ae0b14656a5095
Chayy04 <santosjenny1415@gmail.com>
2024-11-16 00:44:25 +0800

    Added the side-bar to dashboard succesfully together with the dashboard css file

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,7 +8,10 @@ if (!isset($_SESSION['user'])) {
 }
 
 include 'partials/header.php'; // Include header here
+include 'partials/side-bar.php'; // Include sidebar here
+
 ?>
+
 <!-- Template Files here -->
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
     <h1 class="h2">Dashboard</h1>        
