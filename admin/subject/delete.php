c9860cbe929cd0509282f6baa72fd6071475b6ca
Chayy04 <santosjenny1415@gmail.com>
2024-11-17 20:11:53 +0800

    added the form for delete-subject, also i want to add correction for the commit before this: Added a Form in the EDIT SUBJECT is the correct one

---
+++ admin/subject/delete.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user'])) {
+    header("Location: ../index.php");
+    exit();
+}
+
+include '../partials/header.php'; // Include header here
+include '../partials/side-bar.php';
+
+?>
+
+<!-- Template Files here -->
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
+    <h1 class="h2">Delete a Subject</h1>        
+    
+    <div class="row mt-5">
+        <form method="POST" action="" class="border border-secondary-1 p-5 mb-4">
+            <div class="mb-2">
+                <label class="form-label fs-5">Are you sure you want to delete the following subject record?</label> 
+                    <ul style="list-style-type:disc;">
+                        <li><strong>Subject Code:</strong> </li>
+                        <li><strong>Subject Name:</strong> </li>
+                    </ul>
+
+                <!-- Buttons for Submit and Cancel -->
+                <div>
+                    <a href="register.php" class="btn btn-secondary btn-sm">Cancel</a> 
+                    <button type="submit" class="btn btn-primary btn-sm">Delete Subject Record</button>
+                </div>  
+            </div>
+        </form>
+    </div>    
+</main>
+
+<?php
+include '../partials/footer.php'; // Include footer here
+?>
